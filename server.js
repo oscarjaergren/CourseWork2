53754e525baed6a14f03fb092fc5722397b7fc7c
Ubuntu <ubuntu@ip-172-31-58-29.ec2.internal>
2020-12-22 22:20:27 +0000

    Added demonstration message to server.js

--- server.js
+++ server.js
@@ -18,4 +18,5 @@ www.listen(8080,function () {
     host = process.env.HOSTNAME;
     console.log (Started At,startTime,  Running On  ,host, n );
     console.log ('I was here');
+    console.log ('Demonstration of Pipeline')
 });
